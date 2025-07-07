d63eab25f5415f5c7c6afc6695ec46621d584ce9
Tardes <Tardes@CAS-CLO2-16>
2025-07-07 20:43:11 +0200

    Eliminacion de n2 en Clase 1

--- src/es/cursojava/inicio/Clase1.java
+++ src/es/cursojava/inicio/Clase1.java
@@ -6,7 +6,7 @@ public class Clase1 {
 	
 	public static void main (String[] args ) {
 		int edad = 27;
-		System.out.printIn("Mario Ferron 2"+edad);
+		System.out.printIn("Mario Ferron"+edad);
 	}
 
 }
