655c6218dba157a9a0de469047b7ddbf9f54a539
Mariofc97 <mario.ferron1997@gmail.com>
2025-07-08 17:23:43 +0200

    Update Clase1.java desde GitHub

--- src/es/cursojava/inicio/Clase1.java
+++ src/es/cursojava/inicio/Clase1.java
@@ -6,7 +6,8 @@ public class Clase1 {
 	
 	public static void main (String[] args ) {
 		int edad = 27;
-		System.out.printIn("Mario Ferron"+edad);
+		System.out.printIn("Mario Ferron"+edad + "Github");
+		System.out.println("GitHub");
 	}
 
 }
