711531ac3bab5bbb10e49feb0b0592244496e664
Tardes <Tardes@CAS-CLO2-16>
2025-07-10 20:54:04 +0200

    Ejercicio String y validar email

--- src/es/cursojava/inicio/strings/Ejercicio4String.java
+++ src/es/cursojava/inicio/strings/Ejercicio4String.java
@@ -8,21 +8,31 @@ public class Ejercicio4String {
 //		Ejercicio 4
 //		==============================
 //		Escribe un programa que reciba un String con varias palabras separadas por espacios y determine cuál es la palabra más larga.
+//		
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("Introduce la frase que desees: ");
+//		String fraseUsuario = scan.nextLine();
+//		
+//		String[] palabras = fraseUsuario.split(" ");
+//		String palabraMasLarga = "";
+//		
+//		for(int i=0;i<palabras.length;i++){
+//			if (palabras[i].length() > palabraMasLarga.length()) {
+//				palabraMasLarga = palabras[i];
+//			}
+//		}
+//		
+//		System.out.println("La palabra mas larga es: " + palabraMasLarga);
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Introduce la frase que desees: ");
-		String fraseUsuario = scan.nextLine();
+		String frase = "Hola soy Mario sou un paquete";
 		
-		String[] palabras = fraseUsuario.split(" ");
-		String palabraMasLarga = "";
-		
-		for(int i=0;i<palabras.length;i++){
-			if (palabras[i].length() > palabraMasLarga.length()) {
-				palabraMasLarga = palabras[i];
-			}
+		while (frase.strip().contains(" ")) {	
+			int posicionBlanco = frase.indexOf(" ");
+			String palabra = frase.substring(0,posicionBlanco);
+			System.out.println(palabra);
+			frase = frase.substring(posicionBlanco+1);
+			System.out.println(frase);
 		}
-		
-		System.out.println("La palabra mas larga es: " + palabraMasLarga);
 		
 	}
 }
---
+++ src/es/cursojava/inicio/strings/Ejercicio5String.java
@@ -0,0 +1,73 @@
+package es.cursojava.inicio.strings;
+
+import java.util.Scanner;
+
+public class Ejercicio5String {
+	
+//	Ejercicio 5 - Validar un email
+//	==============================
+//	Preguntar al usuario por un email
+//	Le diremos si es valido o no y si no lo es le diremos el motivo
+//
+//	Validaciones: 
+//	+ Que no tenga espacios en blanco x
+//	+ Que tenga @, solo 1 x
+//	+ Que después de la @ tenga al menos un punto x
+//	+ Que entre la @ y el punto a su derecha haya una separación de dos caracateres x
+//	+ Que despues del último punto haya entre 2 y 6 caracteres x
+
+	public static void main(String[] args) {
+
+		while (!validarEmail()) {
+			System.out.println("Inténtalo de nuevo.\n");
+		}
+	}
+	
+	public static boolean validarEmail() {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Introduce tu email: ");
+		String email = scan.nextLine();
+		
+		if (email.contains(" ")) {
+			System.out.println("El email no puede tener espacios en blanco");
+			return false;
+		}
+		
+		int counterArroba = 0;
+		int counterChar = 0;
+		
+		for (int i=0; i<email.length();i++) {
+			if(email.charAt(i) == '@') {
+				counterArroba++;
+			}
+		}
+		
+		if (counterArroba == 0) {
+			System.out.println("Debe de haber al menos un @");
+			return false;
+		} else if (counterArroba > 1) {
+			System.out.println("Solo puede haber un @");
+			return false;
+		}
+		
+		int posArroba = email.indexOf("@");
+		int afterArroba = email.indexOf(".",posArroba);
+		int posUltimoPunto = email.lastIndexOf(".");
+		int charAfterPunto = email.length() - posUltimoPunto - 1;
+		
+		if (afterArroba <= 0) {
+			System.out.println("Despues del @ debe de haber al menos un punto");
+			return false;
+		} else if(afterArroba == posArroba+1 || afterArroba == posArroba+2) {
+			System.out.println("Entre el @ y el punto debe de haber una separacion de dos caracteres");
+			return false;
+		} else if(charAfterPunto<2 || charAfterPunto>6) {
+			System.out.println("Despues del ultimo punto debe de haber entre 2 y 6 caracteres");
+			return false;
+		} else {
+			System.out.println("el email proporcionado se ha validado correctamente");
+			return true;
+		}	
+	}
+}
