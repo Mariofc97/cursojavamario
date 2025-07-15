9b810f408be7f9520ad295296a4fb9d9abede6c1
Mario Ferron <mario.ferron1997@gmail.com>
2025-07-15 13:13:39 +0200

    Cambios realizados en casa en la mañana de 15/07/2025

--- src/es/cursojava/inicio/arrays/ejercicios/Ejercicio3Arrays.java
+++ src/es/cursojava/inicio/arrays/ejercicios/Ejercicio3Arrays.java
@@ -17,54 +17,132 @@ public class Ejercicio3Arrays {
 //		Elige una opción
 //
 
-		System.out.println("Colegio: \n1. Insertar Alumnos. \n2. Mostrar Alumnos. \n3. Buscar Alumno \n4. Borrar Alumno \n5. Salir");
-
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Introduzca unicamente el numero de la opción deseada: ");
-		int opcion = scan.nextInt();
-		scan.nextLine();
-		
-		
-		
-		String [][] colegio = new String [2][3];
+		String[][] colegio = new String[2][3];
+		int opcion;
+		boolean salir = false;
 
-		switch (opcion) {
-		case 1:
-			boolean AlumnoIngresado = false;
-			
-			for (int i = 0; i < colegio.length; i++) {
-				for (int j = 0; j < colegio[i].length; j++) {
-					
-					if (colegio [i][j]==null) {
-						System.out.println("Introduce el nombre del alumno que quiera ingresar: ");
-						String nombreAlumno = scan.nextLine();
-						colegio[i][j]=nombreAlumno;
-						System.out.println("Alumno ingresado en aula " + i + " en el puesto " + j);
-						AlumnoIngresado = true;
+		while (!salir) {
+
+			System.out.println(
+					"Colegio: \n1. Insertar Alumnos. \n2. Mostrar Alumnos. \n3. Buscar Alumno \n4. Borrar Alumno \n5. Salir");
+			System.out.println("Introduzca unicamente el numero de la opción deseada: ");
+			opcion = scan.nextInt();
+			scan.nextLine();
+
+			switch (opcion) {
+			case 1:
+//			1. Insertar Alumnos -> Va a solicitar el nombre de los alumnos para las aulas que tengan puestos disponibles
+
+				while (true) {
+					boolean HayHueco = false;
+
+					for (int i = 0; i < colegio.length; i++) {
+						for (int j = 0; j < colegio[i].length; j++) {
+							if (colegio[i][j] == null) {
+								HayHueco = true;
+								break;
+							}
+						}
+						if (HayHueco)
+							break;
+
+					}
+
+					if (!HayHueco) {
+						System.out.println("No hay mas huecos disponibles.");
 						break;
-					} 
-				
-				}
-			}
-		case 2:
-			for (int i = 0; i < colegio.length; i++) {
-				for (int j = 0; j < colegio[i].length; j++) {
-					System.out.println("\t" + colegio[i][j]);
-				
-				}
-				System.out.println();
-			}
-			
-		case 3:
-			System.out.println("Has elegido Añadir Alumno");
-			break;
-		case 4:
-			System.out.println("Adios!");
-			break;
-		default:
-			System.out.println("La opcion elegida no existe");
+					}
 
+					boolean AlumnoIngresado = false;
+
+					for (int i = 0; i < colegio.length && !AlumnoIngresado; i++) {
+						for (int j = 0; j < colegio[i].length; j++) {
+
+							if (colegio[i][j] == null) {
+								System.out.println("Introduce el nombre del alumno que quiera ingresar: ");
+								String nombreAlumno = scan.nextLine();
+								colegio[i][j] = nombreAlumno;
+								System.out.println("Alumno ingresado en aula " + i + " en el puesto " + j);
+								AlumnoIngresado = true;
+								break;
+							}
+
+						}
+					}
+
+					System.out.println("Desea insertar otro alumno? (s/n): ");
+					String respuesta = scan.nextLine();
+					if (!respuesta.equalsIgnoreCase("s")) {
+						break;
+					}
+				}
+				break;
+			case 2:
+//			2. Mostrar Alumnos -> Muestra los alumnos por aula, si un puesto no tiene alumno, mostrar (vacio o null)
+				for (int i = 0; i < colegio.length; i++) {
+					for (int j = 0; j < colegio[i].length; j++) {
+						System.out.print("\t" + colegio[i][j]);
+
+					}
+					System.out.println();
+				}
+				break;
+
+			case 3:
+//			3. Buscar Alumno -> Solicitar texto, Ju, mostrar aula y puesto en el que se encuentra los alumnos encontrados que contengan esos caracteres
+				System.out.println("Has elegido Buscar Alumno");
+				System.out.println("Introduce tecto para buscar el/los alumnos deseados: ");
+				String busquedaAlumno = scan.nextLine().toLowerCase();
+				boolean encontrado = false;
+				for (int i = 0; i < colegio.length; i++) {
+					for (int j = 0; j < colegio[i].length; j++) {
+						if (colegio[i][j] != null && colegio[i][j].toLowerCase().contains(busquedaAlumno)) {
+							System.out.println("\nEncontrado: " + colegio[i][j] + " en aula " + i + " , puesto " + j);
+							encontrado = true;
+							break;
+						}
+
+					}
+				}
+
+				if (!encontrado) {
+					System.out.println("No se encontro ningun alumno con ese texto");
+				}
+
+				break;
+			case 4:
+//			4. Borrar Alumno -> Solicitar nombre, y se borra a todos los alumnos que tengan ese nombre. HAY QUE PONERLO A NULL, QUE TENGAMOS EN CUENTA QUE EL VALOR POR DEFECTO ES NULL
+				System.out.println("Has elegido Borrar Alumno");
+				System.out.println("Introduce el alumno que deseas eliminar: ");
+				String AlumnoEliminado = scan.nextLine().toLowerCase();
+				boolean eliminado = false;
+				for (int i = 0; i < colegio.length; i++) {
+					for (int j = 0; j < colegio[i].length; j++) {
+						if (colegio[i][j] != null && colegio[i][j].toLowerCase().contains(AlumnoEliminado)) {
+							colegio[i][j] = null;
+							System.out.println("\nEl alumno " + AlumnoEliminado + " se ha eliminado correctamente");
+							eliminado = true;
+							break;
+						}
+					}
+				}
+
+				if (!eliminado) {
+					System.out.println("No se ha encontrado ningun alumno con ese texto");
+				}
+				break;
+			case 5:
+				System.out.println("Saliendo del programa...");
+				System.out.println("\n Adios!!");
+				salir = true;
+				break;
+
+			default:
+				System.out.println("La opcion elegida no existe");
+
+			}
 		}
-	}
 
+	}
 }
