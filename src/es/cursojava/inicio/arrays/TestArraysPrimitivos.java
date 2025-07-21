5f990d578ed419efd39a84774640d555b4fc0c64
Tardes <Tardes@CAS-CLO2-16>
2025-07-21 20:44:37 +0200

    Cambios realizados tarde de 21/07/2025

---
+++ src/es/cursojava/inicio/arrays/TestArraysPrimitivos.java
@@ -0,0 +1,19 @@
+package es.cursojava.inicio.arrays;
+
+public class TestArraysPrimitivos {
+
+	public static void main(String[] args) {
+		
+		int [] numeros =  new int [2];
+		numeros[0] = 20;
+		numeros[1]=20;
+		
+		int edad = numeros [0];
+		System.out.println(edad);
+		edad = 16;
+		System.out.println(numeros[0]);
+		System.out.println(edad);
+
+	}
+
+}
---
+++ src/es/cursojava/inicio/ejercicios/Ejercicio1.java
@@ -0,0 +1,10 @@
+package es.cursojava.inicio.ejercicios;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola don " + args[0]);
+
+	}
+
+}
