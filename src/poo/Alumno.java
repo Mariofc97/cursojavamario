b9f45c6c5e5011fc1f8f7557f901bffe9c1413f2
Tardes <Tardes@CAS-CLO2-16>
2025-09-03 20:48:29 +0200

    tarde 03/09

---
+++ src/poo/Alumno.java
@@ -0,0 +1,74 @@
+package poo;
+
+import java.util.Arrays;
+
+public class Alumno {
+	private String nombre;
+	private String dni;
+	private double notaMedia;
+	private String[] asignaturas;
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getDni() {
+		return dni;
+	}
+
+
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+
+
+	public double getNotaMedia() {
+		return notaMedia;
+	}
+
+
+
+	public void setNotaMedia(double notaMedia) {
+		this.notaMedia = notaMedia;
+	}
+
+
+
+	public String[] getAsignaturas() {
+		return asignaturas;
+	}
+
+
+
+	public void setAsignaturas(String[] asignaturas) {
+		this.asignaturas = asignaturas;
+	}
+
+
+
+	public void estudiar() {
+		System.out.println("El estudiante " + nombre + " tiene " + Arrays.toString(asignaturas));
+		System.out.println("Su nota media es: " + notaMedia);
+		if (notaMedia<5) {
+			System.out.println("Ha estudiado poco");
+		}
+		if (notaMedia>5) {
+			System.out.println("Ha estudiado mucho");
+		}
+		if (notaMedia>9) {
+			System.out.println("Es un genio");
+		}
+	}
+	
+
+}
