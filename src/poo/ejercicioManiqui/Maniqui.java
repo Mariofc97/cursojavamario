2447f46b7ac734faae0e40e546efdd1275c757f1
Tardes <Tardes@CAS-CLO2-16>
2025-09-09 20:43:11 +0200

    Merge branch 'main' of https://github.com/Mariofc97/cursojavamario.git into main

--- src/poo/ejercicioManiqui/Maniqui.java
+++ src/poo/ejercicioManiqui/Maniqui.java
@@ -38,4 +38,44 @@ public class Maniqui {
 		return pantalon == null && camisa == null && vestido == null ;
 	}
 	
+	public double getPrecioRopa() {
+		double precioTotal = 0;
+		Ropa prendas[] = new Ropa[] {this.pantalon, this.camisa, this.vestido}
+		
+		for (Ropa r: prendas) {
+			if (r != null) {
+				precioTotal += r.getPrecio();				
+			}
+		return precioTotal;
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Pantalon getPantalon() {
+		return pantalon;
+	}
+
+	public void setPantalon(Pantalon pantalon) {
+		this.pantalon = pantalon;
+	}
+
+	public Camisa getCamisa() {
+		return camisa;
+	}
+
+	public void setCamisa(Camisa camisa) {
+		this.camisa = camisa;
+	}
+
+	public Vestido getVestido() {
+		return vestido;
+	}
+
+	public void setVestido(Vestido vestido) {
+		this.vestido = vestido;
+	}
+	
 }
--- src/poo/ejercicioManiqui/Tienda.java
+++ src/poo/ejercicioManiqui/Tienda.java
@@ -1,54 +1,76 @@
 package poo.ejercicioManiqui;
 
-public class Tienda{
+public class Tienda {
 
 	public static void main(String[] args) {
+		Tienda tienda1 = new Tienda("Dolce Gabana", 5);
+		Maniqui[] ms = tienda1.abrirTienda();
 		
+		tienda1.abrirTienda();
+		tienda1.vestirManiquies(ms);
+		tienda1.mostrarEscaparate();
 
 	}
-	
-	private String Nombre;
-	private String direccion;
+
+	private String nombre;
 	private Maniqui[] maniquis;
 	private int numManiquis;
 
-	
-	public Tienda(String nombre, String direccion, int numManiquis) {
-		this.Nombre = nombre;
-		this.direccion = direccion;
+	public Tienda(String nombre, int numManiquis) {
+		this.nombre = nombre;
 		this.maniquis = new Maniqui[numManiquis];
 	}
-	
-	public void abrirTienda() {
-		Vestido vestido1 = new Vestido("Rojo", 45.0,"L");
-		Vestido vestido2 = new Vestido("Blanco", 85.0,"M");
-		
+
+	public Maniqui[] abrirTienda() {
+
+		Vestido vestido1 = new Vestido("Rojo", 45.0, "L");
+		Vestido vestido2 = new Vestido("Blanco", 85.0, "M");
+
 		Maniqui maniqui1 = new Maniqui(01, vestido1);
 		Maniqui maniqui2 = new Maniqui(02, vestido2);
 		Maniqui maniqui3 = new Maniqui(03);
 		
+		Maniqui[] maniquies = {maniqui1, maniqui2, maniqui3};
+
 		numManiquis = 3;
-	}
-	
-	public void vestirManiquies() {
 		
-		for (int i = 0; i < numManiquis; i++) {
+
+		System.out.println("La tienda " + nombre + " ha abierto con " + numManiquis + " maniquies");
+		
+		return maniquies;
+	}
+
+	public void vestirManiquies(Maniqui[] maniquiesVestir) {
+
+		for (Maniqui maniqui : maniquis) {
 			Maniqui m = maniquis[i];
 			if (maniquis[i] != null && m.desvestir()) {
-				Boton boton1 = new Boton("Negro","pequeño", "redondo");
-				Boton boton2 = new Boton("Blanco","grande", "cuadrado");
-				Boton botonesPantalon[] = {boton1,boton2};
-				Boton botonesCamisa[] = {boton2, boton2, boton2, boton2, boton2};
-				Pantalon p = new Pantalon("Marron", 50.00,"XL",botonesPantalon);
-				Camisa c = new Camisa("Azul",45.00,"L",botonesCamisa);
-				m.vestir(p,c);
-				
-				
-				
+				Boton boton1 = new Boton("Negro", "pequeño", "redondo");
+				Boton boton2 = new Boton("Blanco", "grande", "cuadrado");
+				Boton botonesPantalon[] = { boton1, boton2 };
+				Boton botonesCamisa[] = { boton2, boton2, boton2, boton2, boton2 };
+				Pantalon p = new Pantalon("Marron", 50.00, "XL", botonesPantalon);
+				Camisa c = new Camisa("Azul", 45.00, "L", botonesCamisa);
+				m.vestir(p, c);
+
+				System.out.println("Se ha vestido el maniqui con id: " + (i + 1) + " con camisa y pantalón");
 			}
-			
-			
 		}
+
+	}
+	
+	public void mostrarEscaparate(Maniqui[] maquinis) {
+		double total = 0;
+		System.out.println("Escaparate de la tienda: ");
+		for (int i = 0; i < numManiquis; i++) {
+			Maniqui m = maniquis[i];
+			if (m != null) {
+				total += m.getPrecioRopa();
+			}
+		}
+		
+		System.out.println("El precio total del escaparate teniendo en cuenta todos los maniquis es: " + total + " €");
+		
 	}
 
 }
