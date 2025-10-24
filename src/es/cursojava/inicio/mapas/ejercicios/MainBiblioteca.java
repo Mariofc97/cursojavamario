8ccbac5f15d5b828d8b235cc67cf8043952ea2da
Mario Ferron <mario@email.com>
2025-10-24 18:28:50 +0200

    cambios 24/10/2025

--- src/es/cursojava/inicio/mapas/ejercicios/MainBiblioteca.java
+++ src/es/cursojava/inicio/mapas/ejercicios/MainBiblioteca.java
@@ -1,59 +1,133 @@
 package es.cursojava.inicio.mapas.ejercicios;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import utils.Utilidades;
+
 public class MainBiblioteca {
 
 	public static void main(String[] args) {
-		
-		Map<Autor,List<Libro>> biblioteca = new HashMap<>();
-		
-		Libro libro1 = new Libro("Lololo","ISB457",2000);
-		Libro libro2 = new Libro("Lelele","ISB458",2010);
-		Libro libro3 = new Libro("lilili","ISB459",2011);
-		Libro libro4 = new Libro("Lululu","ISB450",2005);
-		Libro libro5 = new Libro("Tralari","ISB456",2007);
-		Libro libro6 = new Libro("El mejor libro del mundo","ISB454",2009);
-		Libro libro7 = new Libro("Phantom's opera","ISB455",2014);
-		Libro libro8 = new Libro("Mimotoalpinaderrapante","ISB453",2015);
-		Libro libro9 = new Libro("Bojack Horseman","ISB452",2015);
-		
-		List<Libro> libros = Arrays.asList(libro1,libro2,libro3,libro4,libro5,libro6,libro7,libro8,libro9);
-		
-		Autor autor1 = new Autor("Mario Ferron","española");
-		Autor autor2 = new Autor("Ayumu Miyamoto","japonesa");
-		Autor autor3 = new Autor("Josephine Smith","canadiense");
-		
+
+		Libro libro1 = new Libro("Lololo", "ISB457", 2000);
+		Libro libro2 = new Libro("Lelele", "ISB458", 2010);
+		Libro libro3 = new Libro("lilili", "ISB459", 2011);
+		Libro libro4 = new Libro("Lululu", "ISB450", 2005);
+		Libro libro5 = new Libro("Tralari", "ISB456", 2007);
+		Libro libro6 = new Libro("El mejor libro del mundo", "ISB454", 2009);
+		Libro libro7 = new Libro("Phantom's opera", "ISB455", 2014);
+		Libro libro8 = new Libro("Mimotoalpinaderrapante", "ISB453", 2015);
+		Libro libro9 = new Libro("Bojack Horseman", "ISB452", 2015);
+
+		List<Libro> librosTotales = Arrays.asList(libro1, libro2, libro3, libro4, libro5, libro6, libro7, libro8,
+				libro9);
+		List<Libro> libros1 = Arrays.asList(libro1, libro2);
+		List<Libro> libros2 = Arrays.asList(libro3, libro4, libro5, libro6);
+		List<Libro> libros3 = Arrays.asList(libro7, libro8, libro9);
+
+		Autor autor1 = new Autor("Mario Ferron", "española");
+		Autor autor2 = new Autor("Ayumu Miyamoto", "japonesa");
+		Autor autor3 = new Autor("Josephine Smith", "canadiense");
+
 		List<Autor> autores = Arrays.asList(autor1, autor2, autor3);
-		
+
+		Map<Autor, List<Libro>> biblioteca = new HashMap<>();
+		biblioteca.put(autor1, libros1);
+		biblioteca.put(autor2, libros2);
+		biblioteca.put(autor3, libros3);
+
 		mostrarDatosAutores(autores);
-		mostrarTitulosMas2010(libros);
-		
+		mostrarTitulosMas2010(librosTotales);
+		mostrarAutoresConMasDeDosLibros(biblioteca);
+		pideDatosBiblioteca(biblioteca);
 
 	}
-	
+
+	// + mostrar las nacionalidades de los autores
 	private static void mostrarDatosAutores(List<Autor> autores) {
-		
+
 		System.out.println("Datos autores: ");
 		for (Autor autor : autores) {
 			System.out.println("Nombre: " + autor.getNombre());
 			System.out.println("Nacionalidad: " + autor.getNacionalidad());
 		}
 	}
-	
+
+	// + mostrar el título de todos los libros publicados después del 2010
 	private static void mostrarTitulosMas2010(List<Libro> libros) {
 		System.out.println("Mostrar titulos publicados despues de 2010: ");
 		for (Libro l : libros) {
-			if (l.getAnio()>2015) {
+			if (l.getAnio() > 2015) {
 				System.out.println("Titulo: " + l.getTitulo());
 				System.out.println("Año: " + l.getAnio());
 			}
 		}
 	}
+
+	// mostrar por cada autor que tenga más de dos libros su nombre y el nombre de
+	// todos los libros que tiene publicados
+	private static void mostrarAutoresConMasDeDosLibros(Map<Autor, List<Libro>> biblioteca) {
+		System.out.println("---------------------------");
+		for (Map.Entry<Autor, List<Libro>> entrada : biblioteca.entrySet()) {
+
+			Autor autor = entrada.getKey();
+			List<Libro> librosAutor = entrada.getValue();
+
+			if (librosAutor.size() > 2) {
+				System.out.println("Autor: " + autor.getNombre());
+				System.out.println("Nacionalidad: " + autor.getNacionalidad());
+
+				for (Libro libro : librosAutor) {
+					System.out.println("	Titulo: " + libro.getTitulo());
+				}
+
+			}
+			System.out.println("---------------------------");
+
+		}
+	}
 	
+	//+ Solicitar los datos de un libro, autor, titulo, isbn y año de publicación, 
+	//- si el autor no está en la biblioteca, solicitar la nacionalidad del autor y meterlo en la biblioteca, 
+	//- si el autor sí está ya, agregar el libro a su listado
 	
+	private static void pideDatosBiblioteca(Map<Autor, List<Libro>> biblioteca) {
+		String autorDeseado = Utilidades.pideDatoCadena("Introduce el autor del libro buscado: ");
+		String tituloDeseado = Utilidades.pideDatoCadena("Introduce el libro que buscas: ");
+		String isbnLibroDeseado = Utilidades.pideDatoCadena("Introduce el ISBN del libro que buscas: ");
+		int anioLibroDeseado = Integer.parseInt(Utilidades.pideDatoCadena("Introduce el ISBN del libro que buscas: "));
+		
+		Libro libroNuevo = new Libro(tituloDeseado,isbnLibroDeseado,anioLibroDeseado);
+		
+		Autor autorEncontrado = null;
+		
+		for(Autor a : biblioteca.keySet()) {
+			if(a.getNombre().equalsIgnoreCase(autorDeseado)) {
+				a = autorEncontrado;
+				break;
+			}
+		}
+		
+		if (autorEncontrado != null){
+			List <Libro>librosAutor = biblioteca.get(autorEncontrado);
+			librosAutor.add(libroNuevo);
+			System.out.println("Libro añadido al autor existente " + autorEncontrado.getNombre());
+			
+		} else {
+			String nacionalidadAutor = Utilidades.pideDatoCadena("Autor no encontrado. Introduce la nacionalidad del autor " + autorDeseado + " :");
+			
+			Autor autorNuevo = new Autor(autorDeseado,nacionalidadAutor);
+			
+			List<Libro> nuevaListaLibros = new ArrayList<>();
+			nuevaListaLibros.add(libroNuevo);
+			
+			biblioteca.put(autorNuevo, nuevaListaLibros);
+			
+			System.out.println("Autor y libro añadidos a la biblioteca: " + autorNuevo.getNombre());
+		}
+	}
 
 }
--- src/es/cursojava/inicio/mapas/ejercicios/MapaColegio.java
+++ src/es/cursojava/inicio/mapas/ejercicios/MapaColegio.java
@@ -1,5 +1,6 @@
 package es.cursojava.inicio.mapas.ejercicios;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -8,40 +9,53 @@ import java.util.Map;
 import java.util.Set;
 
 import poo.Alumno;
+import poo.Persona;
 
 public class MapaColegio {
 
 	public static void main(String[] args) {
-		List<Alumno> alumnos = generaAlumnos();
+		Colegio colegio1 = new Colegio("Cole1", "Dir1");
+		Colegio colegio2 = new Colegio("Cole2", "Dir2");
 		
-		Map<String,List<Alumno>> aulas = new HashMap<>();
-		aulas.put("aula1",alumnos.subList(0, 2));
-		aulas.put("aula2",alumnos.subList(2, 3));
-		aulas.put("aula3",alumnos.subList(3, 7));
 		
-		List<Alumno> alumnosAula1 = aulas.get("aula3");
-		System.out.println(alumnosAula1.size());
-		System.out.println("====================================");
-		Collection<List<Alumno>> listasAlumnos = aulas.values();
-		int totalAlumnos = 0;
-		for (List<Alumno> list : listasAlumnos) {
-			totalAlumnos += list.size();
-			System.out.println("Tamaño: "+list.size());
-			for (Alumno a : list) {
-				System.out.println(a);
+		List<Alumno> alumnos = MapaColegio.generaAlumnos();
+		
+		Map<String,List<Alumno>> aulasCole3 = new HashMap<>();
+		aulasCole3.put("aula1",alumnos.subList(0, 2));
+		aulasCole3.put("aula2",alumnos.subList(2, 3));
+		aulasCole3.put("aula3",alumnos.subList(3, 7));
+		Colegio colegio3 = new Colegio("Cole3", "Dir3",aulasCole3);
+		
+		Map<String,List<Alumno>> aulasCole2 = new HashMap<>();
+		aulasCole2.put("aula1", alumnos.subList(7, 10));
+		colegio2.setAulas(aulasCole2);
+		
+		
+		List<Colegio> colegios = Arrays.asList(colegio1,colegio2, colegio3);
+		for (Colegio colegio : colegios) {
+			if (colegio.getAulas().isEmpty()) {
+				System.out.println(colegio.getNombre() +" sin aulas");
+				Map<String,List<Alumno>> aulasCole1 = new HashMap<>();
+				aulasCole1.put("aula1", alumnos.subList(10, 11));
+				aulasCole1.put("aula2", alumnos.subList(11, 12));
+				aulasCole1.put("aula3", alumnos.subList(12, 13));
+				
+				colegio.setAulas(aulasCole1);
 			}
 		}
-		System.out.println("Tamaño total: "+totalAlumnos);
+		Alumno alumnoN = new Alumno("AN", "222N", 9.5);
+		Alumno alumnoN1 = new Alumno("AN1", "222N1", 6.5);
+		//colegio2.getAulas().get("aula1").add(alumnoN);//Añadimos alumnos a un aula que ya existe
 		
-		
-		Set<String> nombreAulas = aulas.keySet();
-		for (String aula : nombreAulas) {
-			System.out.print(aula);
-			System.out.println(aulas.get(aula).size());
-		}
-		
+		colegio2.getAulas().put("aula2", new ArrayList(Arrays.asList(alumnoN)));
+		colegio2.getAulas().get("aula2").add(alumnoN1);
+		//System.out.println(colegio2.getAulas().get("aula2").size());
+
+		mostrarDatosColegios(colegios);
+		mostrarColegioNotaMediaMayor(colegios);
 	}
-	private static List<Alumno> generaAlumnos(){
+
+	private static List<Alumno> generaAlumnos() {
 		Alumno alumno1 = new Alumno("A1", "111A", 7.5);
 		Alumno alumno2 = new Alumno("A2", "222B", 2.5);
 		Alumno alumno3 = new Alumno("A3", "333C", 3.5);
@@ -49,10 +63,66 @@ public class MapaColegio {
 		Alumno alumno5 = new Alumno("A5", "555E", 5.5);
 		Alumno alumno6 = new Alumno("A6", "666F", 6.5);
 		Alumno alumno7 = new Alumno("A7", "777G", 8.5);
-		
-		List<Alumno> alumnos = Arrays.asList(alumno1,alumno2,alumno3,alumno4,alumno5,alumno6,alumno7);
-		
+		Alumno alumno8 = new Alumno("A8", "888G", 8.5);
+		Alumno alumno9 = new Alumno("A9", "999G", 8.5);
+		Alumno alumno10 = new Alumno("A10", "1010G", 9.5);
+		Alumno alumno11= new Alumno("A11", "1011G", 9.75);
+		Alumno alumno12= new Alumno("A12", "1012G", 8.0);
+		Alumno alumno13= new Alumno("A13", "1013G", 7.7);
+
+
+		List<Alumno> alumnos = Arrays.asList(alumno1, alumno2, alumno3, alumno4, alumno5, alumno6, alumno7,alumno8,alumno9,alumno10,alumno11,alumno12,alumno13);
+
 		return alumnos;
+	}
+
+	private static void mostrarColegioNotaMediaMayor(List<Colegio> colegios) {
+		
+		double notaMediaMaxima = 0;
+		Colegio mejorColegio = null;
+		for(Colegio colegio : colegios) {
+			double notaMediaColegio = 0;
+			int contadorAlumnos = 0;
+			System.out.println(colegio.getNombre());
+			Map<String, List<Alumno>> aulas = colegio.getAulas();
+			Collection<List<Alumno>> listadosAlumnos = aulas.values();
+			for (List<Alumno> alumnos : listadosAlumnos) {
+				for (Alumno a : alumnos) {
+					notaMediaColegio += a.getNotaMedia();
+					contadorAlumnos++;
+				}
+				
+			}
+			
+			notaMediaColegio = notaMediaColegio/contadorAlumnos;
+			System.out.println(notaMediaColegio);
+			if (notaMediaColegio>notaMediaMaxima) {
+				notaMediaMaxima = notaMediaColegio;
+				mejorColegio = colegio;
+			}
+		}
+		
+		System.out.println(mejorColegio.getNombre() + " tienes la nota media más alta");
+		
+		Map<Integer, Persona> datos = new HashMap<>();
+		Collection Persona = datos.values();
+	}
+
+	private static void mostrarDatosColegios(List<Colegio> colegios) {
+
+		for (Colegio colegio : colegios) {
+			System.out.println(colegio);
+			Map<String, List<Alumno>> aulas = colegio.getAulas();
+			Set<String> nombresAulas = aulas.keySet();
+			for (String nombreAula : nombresAulas) {
+				System.out.println("\t" + nombreAula);
+				List<Alumno> alumnos = aulas.get(nombreAula);
+				for (Alumno alumno : alumnos) {
+					System.out.println("\t\t" + alumno);
+				}
+			}
+		}
+
 	}
 
 }
