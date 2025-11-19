abfde450c18727a56030514a452e04b4096efeff
Mario Ferron <mario@email.com>
2025-11-19 20:52:02 +0100

    CAMBIOS TARDE 19/11/2025

---
+++ src/examen/ficheros/correcion/Parte2LecturaFichero.java
@@ -0,0 +1,41 @@
+package examen.ficheros.correcion;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Parte2LecturaFichero {
+
+	private static final int contador = 0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Parte2LecturaFichero parte2 = new Parte2LecturaFichero();
+		try {
+			List<String> lines =parte2.leerArchivo3("./recursos/application.log");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<String> leerArchivo3(String nombre) throws IOException {
+		Path path = Paths.get(nombre);
+
+		List<String> lines = Files.readAllLines(path);
+
+		return lines;
+	}
+	
+	public void contarPalabraJava(String linea) {
+		String [] palabras = linea.split(" ");
+		for (String palabra : palabras) {
+			if(palabra.contains("java")) {
+				this.contador++;
+			}
+		}
+		System.out.println(linea);
+	}
+
+}
